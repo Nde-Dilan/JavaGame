3bfe371043484f45b12e039578911bbf05d10811
Nde Dilane <nde.dilan@ictuniversity.edu.cm>
2023-11-18 10:55:03 +0100

    Creating two files :Heelo.java and README

---
+++ Hello.java
@@ -0,0 +1,9 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
+
+//0011
+
+//A JDK is a software package that contains libraries, tools for developing and testing Java applications (development tools), and tools for running applications on the Java platform (Java Runtime Environment — JRE).

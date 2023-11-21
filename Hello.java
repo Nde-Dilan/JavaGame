e877c5139c135ac95cdcb5f40d7a40e03b5d6e28
Nde Dilane <nde.dilan@ictuniversity.edu.cm>
2023-11-21 21:51:12 +0100

    ...

--- Hello.java
+++ Hello.java
@@ -4,6 +4,9 @@ public class Hello {
     }
 }
 
+//Untracked(Unstaged) -> Staged -> Committed
+
+//git add -> git commit -> git push
 //0011
 
 //A JDK is a software package that contains libraries, tools for developing and testing Java applications (development tools), and tools for running applications on the Java platform (Java Runtime Environment — JRE).

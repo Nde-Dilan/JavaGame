d7a4f33c47a2401cb47acedea4e69c46e6c94547
Nde Dilane <nde.dilan@ictuniversity.edu.cm>
2023-11-22 14:31:42 +0100

    Adding the files of the 2nd session

--- Hello.java
+++ Hello.java
@@ -1,12 +1,43 @@
+/*
+ * The purpose of this project is to create a text-based role-playing game in the terminal.
+•	The game features two players: you and an enemy.
+•	Both of you start with 50 hit points.
+•	Your character has 3 potions that allow you to recover hit points. The enemy has no potions. Each potion allows you to recover a random number of hit points, ranging from 9 to 20.
+•	Your attack inflicts random damage on the enemy, between 5 and 10 hit points.
+•	The enemy's attack inflicts random damage on you, between 5 and 15 hit points.
+•	When you use a potion, you skip the next turn.
+Gameplay:
+When you launch the script, you should ask the user whether they want to attack (1) or use a potion (2):
+"Do you want to attack (1) or use a potion (2)?"
+This sentence will be asked to the user at the beginning of each turn. If the user chooses the first option (1), you inflict damage on the enemy. This damage will range from 5 to 10 hit points and will be randomly determined by the program. If option (2) is chosen, the player receives a certain number of hit points, between 9 and 20, and their turn is skipped.
+
+ */
+
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
 }
 
-//Untracked(Unstaged) -> Staged -> Committed
+// Untracked(Unstaged) -> Staged -> Committed
 
-//git add -> git commit -> git push
-//0011
+// git add -> git commit -> git push
+// 0011
 
-//A JDK is a software package that contains libraries, tools for developing and testing Java applications (development tools), and tools for running applications on the Java platform (Java Runtime Environment — JRE).
+// A JDK is a software package that contains libraries, tools for developing and
+// testing Java applications (development tools), and tools for running
+// applications on the Java platform (Java Runtime Environment — JRE).
+
+/*
+ * git pull origin main -> is to pull the latest changes from the remote
+ * repository
+ * git pull origin main --allow-unrelated-histories -> is to pull the latest
+ * changes from the remote repository if the first one shows an error(refusing
+ * to merge unrelated histories)
+ * Maybe you will need to accept the changes inside VScode
+ * git add -A -> is to add all the files to the staging area
+ * git commit -m "Fixing conflicts"
+ * git push -u origin main -> is to push the latest changes to the remote
+ * repository
+ * Now you are done.
+ */
---
+++ Person.java
@@ -0,0 +1,39 @@
+/*
+ * What's a class?
+ * A class is a blueprint for creating objects.
+ */
+
+
+public class Person {
+    // Attributes
+    private String name;        // Name of the person
+    private int age;            // Age of the person
+    private String occupation;  // Occupation of the person
+
+    // Constructor
+    // This is a special method used for creating an instance of the Person class.
+    // It initializes the attributes with the provided values when a new Person object is created.
+    public Person(String name, int age, String occupation) {
+        this.name = name;
+        this.age = age;
+        this.occupation = occupation;
+    }
+
+    // Method for walking
+    // This method represents an action that a person can perform, in this case, walking.
+    // It prints a message indicating that the person is walking.
+    public void walk() {
+        System.out.println(name + " is walking.");
+    }
+
+    // Method for eating
+    // This method represents another action that a person can perform, in this case, eating.
+    // It prints a message indicating that the person is eating.
+    public void eat() {
+        System.out.println(name + " is eating.");
+    }
+
+    public void presentYourSelf(){
+        System.out.println("Hello, my name is " + name + " and I am " + age + " years old. I am a " + occupation + ".");
+    }
+}
